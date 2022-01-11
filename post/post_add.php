9fa9d875f68eef514695be573c5b22547922725b
Dangar2011 <1951060586@e.tlu.edu.vn>
2022-01-11 15:47:38 +0700

    dvdang update_post

--- post/post_add.php
+++ post/post_add.php
@@ -1,22 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create Post</title>
-</head>
-
-<body>
-
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Create Post</title>
+    </head>
     <body>
         <form action="post_load.php" enctype="multipart/form-data" method="post" class="form" >
             <table width="600" height="600" border="1" cellspacing="5" cellpadding="5" align="center">
                 <tr>
                     <td width="230">Title: </td>
                     
-                    <td width="550"><input type="text" name="txtTitle" /></td>
+                    <td><input style="height:40px;width:99%;" type="text" name="txtTitle" /></td>
                 </tr>
                 <tr>
                     <td>Content: </td>
@@ -26,13 +23,17 @@
                 <tr>
                     <td>Image: </td>
                     <td>
-                        <input type="file" name="txtImage" class="imgPost"><br /><br />
+                        <input style="width:99%;"type="file" name="txtImage" class="imgPost"><br /><br />
                     </td>
+                </tr>
+                <tr>
+                    <td>Address: </td>
+                    <td><input style="height:40px;width:99%;" type="text" name="txtAddress" class="Diachi"><br /><br /></td>
                 </tr>
 
                 <tr>
                     <td>Openday</td>
-                    <td> <input style="height:40px;width:100%;" type="date" id="openday" name="txtOpenday"></input></td>
+                    <td> <input style="height:40px;width:99%;" type="date" id="openday" name="txtOpenday"></input></td>
                 </tr>
                 <tr>
                     <td colspan="2" align="center"><input type="submit" name="btn_Upload" value="Add Post" /></td>
@@ -42,6 +43,6 @@
         <h2>Old Post </h2>
         <?php //require 'C:\xampp\htdocs\CongNgheWeb_Team25\post\post_load.php';?>
     </body>
-</body>
+
 
 </html>
