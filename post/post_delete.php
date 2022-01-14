ab1480f32954c64315ed5f9bb0b7471a60199be6
trongbang1501 <95405405+trongbang1501@users.noreply.github.com>
2022-01-14 14:49:52 +0700

    ptbang
    B

--- post/post_delete.php
+++ post/post_delete.php
@@ -3,11 +3,12 @@ include '../post/post_connect.php';
 if(isset($_REQUEST['id']) and $_REQUEST['id']!=""){
 $id=$_GET['id'];
 $sql = "DELETE FROM posts WHERE idPost='$id'";
-$delete=mysqli_query($conn,$delete)
+$delete=mysqli_query($conn,$delete);
 
 if($delete){//kiểm tra việc query thành công
     echo "Delete successful!";
-    }else{
+    }
+    else{
     echo = "File upload failed, please try again.";
 
 }
