52525237ba4129f1a25845272e22e4178d663cd7
trongbang1501 <95405405+trongbang1501@users.noreply.github.com>
2022-01-15 02:39:39 +0700

    ptbang
    B

--- events.php
+++ events.php
@@ -17,7 +17,7 @@
         <div class="top_narbar">
             <nav class="navbar navbar-expand-lg navbar-light navbar-fixed-top">
                 <div class="container-fluid">
-                    <a class="navbar-brand ms-4" href="home.php"><img src="image/meetup-logo-1.png" style="height: 32px; width: 100px;" alt=""></a>
+                    <a class="navbar-brand ms-4" href="../CongNgheWeb_Team25/index.php"><img src="image/meetup-logo-1.png" style="height: 32px; width: 100px;" alt=""></a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
